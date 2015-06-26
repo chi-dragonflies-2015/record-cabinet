6946c193839c1ebb0aaa1cf0eff2c54f91ef6b45
Amber Taniuchi <ambrela@gmail.com>
2015-06-25 22:16:51 -0500

    add search logic and beginning of ajax request

--- app/controllers/records.rb
+++ app/controllers/records.rb
@@ -1,11 +1,31 @@
-post '/albums' do
-  @album = Record.search_albums(params[:album])
-  @artist_name = Record.artist_name
-  @release_date = Record.release_date
-  @image = Record.image
-  if request.xhr?
-
-  else
-
-  end
+get '/search' do
+  erb :'/records/search'
 end
+
+
+post '/search' do
+    @albums = Record.search_albums(params[:album])
+    if request.xhr?
+      puts "$$$$$$$$$$$$$$$$$$$$$$$$$$$$"
+      puts @albums.first.name
+      puts @albums.first.release_date
+      puts "$$$$$$$$$$$$$$$$$$$$$$$$$$$$"
+      # erb :'/records/_results', :locals => { albums: @albums }
+    else
+      erb :'/records/search'
+    end
+end
+
+
+
+# post '/albums' do
+#   @album = Record.search_albums(params[:album])
+#   @artist_name = Record.artist_name
+#   @release_date = Record.release_date
+#   @image = Record.image
+#   if request.xhr?
+
+#   else
+
+#   end
+# end
